75b6e1870f57b4b95524853fbb643b031042faf2
ronin4net <stanley751@gmx.net>
2016-12-31 15:25:09 +0100

    help system improved

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,9 +1,13 @@
-## 1.1 - Unreleased
+## 1.2 - Unreleased
 
-* API doc generation target added
-* AssemblyInfo generation added (if ChangeLog.md exists)
-* NUnit3 support added
-* App icons added
-* build workflow modeling and execution completely handled by FAKE
-* Support for creation and publishing of NuGet packages
-* Support for publishing releases to GitHub
+- Help system (Build Definition) improved
+
+## 1.1 - 2016-12-31
+
+- API doc generation target added
+- AssemblyInfo generation added (if ChangeLog.md exists)
+- NUnit3 support added
+- App icons added
+- build workflow modeling and execution completely handled by FAKE
+- Support for creation and publishing of NuGet packages
+- Support for publishing releases to GitHub
--- src/Plainion.CI/Views/BuildDefinitionView.xaml.cs
+++ src/Plainion.CI/Views/BuildDefinitionView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Linq;
 
 namespace Plainion.CI.Views
 {
@@ -7,6 +11,35 @@ namespace Plainion.CI.Views
         public BuildDefinitionView()
         {
             InitializeComponent();
+
+            EventManager.RegisterClassHandler( typeof( UIElement ), Keyboard.PreviewGotKeyboardFocusEvent, ( KeyboardFocusChangedEventHandler )OnPreviewGotKeyboardFocus );
+        }
+
+        private void OnPreviewGotKeyboardFocus( object sender, KeyboardFocusChangedEventArgs e )
+        {
+            var control = sender as FrameworkElement;
+            if( control == null )
+            {
+                return;
+            }
+
+            foreach( var child in Help.Children.OfType<FrameworkElement>() )
+            {
+                child.Visibility = Visibility.Collapsed;
+            }
+
+            if( control.Tag == null )
+            {
+                return;
+            }
+
+            var help = Help.Children.OfType<FrameworkElement>().SingleOrDefault( c => c.Name == control.Tag.ToString() );
+            if( help == null )
+            {
+                return;
+            }
+
+            help.Visibility = Visibility.Visible;
         }
     }
 }
